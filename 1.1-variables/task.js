e3f40374e8892e928ed510b37d12d9be6e862e73
Ekaterina Volkova <volkovakaterin@gmail.com>
2020-10-08 19:04:41 +0400

    task.js changed

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,31 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+	let algebra = a;
+   let geography = g;
+   let physics = p;
+
+   let averageGrade = (algebra + geography + physics) / 3;
     
-	return // ваш ответ;
+	return averageGrade;
 }
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+   let myName = userName;
+   let message = "Привет, мир! Меня зовут " + myName;
+
     
-	return // ваш ответ;
+	return message;
 }
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+	let x = 2;
+    let y = 22;
+    let z = 0;
+	
+	let result = x * y + 5 * z + x - 1;
+
+    return result;
 }
