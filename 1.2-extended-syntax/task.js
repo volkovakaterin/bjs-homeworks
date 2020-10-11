5a071ccdb5299bb23f1cb3158d5e798f92aad020
Ekaterina Volkova <volkovakaterin@gmail.com>
2020-10-11 23:03:30 +0400

    task.js changed

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,12 +1,39 @@
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+    let discriminant = b^2 - 4*a*c;
+    let x;
+    let x3 = -b / (2*a); 
+    let x1 = (-b + Math.sqrt(discriminant)) / (2*a);
+    let x2 = (-b - Math.sqrt(discriminant)) / (2*a);
+    
+    if (discriminant > 0){
+        x = [x1,x2];
+    }
+    else if (discriminant = 0){
+        x = [x3];
+    }
+    else {
+        x = [];  
+    } 
+    return x;
 }
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
-}
+    let = averageMark;
+    
+    if (marks.length > 5){
+    marks = marks.slice(0,5);
+    } 
+    for (let i = 0; i < marks.length; i++){
+    averageMark = averageMark + marks[i];
+    }
+    averageMark = averageMark / marks.length;
+     if (marks.length === 0){
+    averageMark = 0; 
+    }
+     return averageMark;
+    }
 
 function askDrink(name,dateOfBirthday){
     // код для задачи №3 писать здесь
