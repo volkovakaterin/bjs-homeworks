24ca7d5b2efa7b6f75f83785288afb6f9f0f9dea
Ekaterina Volkova <volkovakaterin@gmail.com>
2020-10-13 16:23:16 +0400

    task.js changed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,14 +1,22 @@
+"use strict";
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    "use strict";
-    let credit = amount - contribution;
-    let endOfCredit = new Date(date); 
-    let months = endOfCredit.getMonth() - new.Date().getMonth() +(12 * (endOfCredit.getFullYear() - new.Date().getFullYear()));
-    console.log(months);
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let credit = amount - contribution; 
+    let months = date.getMonth() - new Date().getMonth() +(12 * (date.getFullYear() - new Date().getFullYear()));
+    let P = (percent / 100) / 12;
+    let payment= credit*(P+P/( Math.pow((1+P),months)-1));
+    let totalAmount = contribution + (payment * months);
+    totalAmount = totalAmount.toFixed(2);
+    console.log(totalAmount);
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    let greeting;
+    if (name === null || name === undefined || name === ""){
+        greeting = "Привет, мир! Меня зовут Аноним"
+    } 
+    else {
+        greeting = `Привет, мир! Меня зовут ${name}`
+    }
+    return greeting;
 }
