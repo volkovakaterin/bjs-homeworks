d845af512d9ba281679c5ed4027c5533d6a36a81
Ekaterina Volkova <volkovakaterin@gmail.com>
2020-10-16 20:52:51 +0400

    task.js changed

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -5,18 +5,36 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     let P = (percent / 100) / 12;
     let payment= credit*(P+P/( Math.pow((1+P),months)-1));
     let totalAmount = contribution + (payment * months);
-    totalAmount = totalAmount.toFixed(2);
+    function numericalConversion(value) {
+      if (isNaN(value)){
+        value = Number(value);
+      }
+    } 
+    numericalConversion(percent);
+    numericalConversion(contribution);
+    numericalConversion(amount);
+    if (isNaN(percent)){
+      console.log(`Параметр percent содержит неправильное значение ${percent}`);  
+    }
+    if (isNaN(contribution)){    
+      console.log(`Параметр contribution содержит неправильное значение ${contribution}`);
+    }
+    if (isNaN(amount)){
+      console.log(`Параметр amount содержит неправильное значение ${amount}`);
+     }
+     
+
+     totalAmount = totalAmount.toFixed(2);
+     totalAmount = Number(totalAmount);
     console.log(totalAmount);
     return totalAmount;
 }
 
 function getGreeting(name) {
-    let greeting;
-    if (name === null || name === undefined || name === ""){
-        greeting = "Привет, мир! Меня зовут Аноним"
+    let greeting = `Привет, мир! Меня зовут ${name}`;
+    if (name === "null" || name === "undefined" || name === `""` || name == false){
+        greeting = "Привет, мир! Меня зовут Аноним";
     } 
-    else {
-        greeting = `Привет, мир! Меня зовут ${name}`
-    }
+    
     return greeting;
 }
