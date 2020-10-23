b726d71fce59b68a9477458ae1e28dae0ea6f16c
Ekaterina Volkova <volkovakaterin@gmail.com>
2020-10-23 19:12:17 +0400

    task.js changed

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -3,38 +3,23 @@ String.prototype.isPalindrome = function () {
     let  lowerCase = this.toLowerCase().replace(/\s/g,'');
     return lowerCase === lowerCase.split('').reverse().join('');
   }
-
-function getAverageMark(marks) {
-    for (var prop in marks){
-       let averageMark = 0;
-       if (marks.prop.length === 0){
-       return averageMark
-       }
-       else { 
-       for (let i = 0; i < marks.prop.length; i++){
-       averageMark = averageMark + marks.prop[i];
-       }
-       averageMark = averageMark / marks.prop.length;
-        return averageMark;
-       }
+    
+function getAverageMark(marks){
+   let averageMark = 0;
+   if (marks.length === 0){
+      return averageMark
    }
-   for (var prop in marks){
-       let average = 0;
-       if (marks.length === 0){
-        return average
-        }
-        else { 
-        for (let i = 0; i < marks.length; i++){
-        average = average + marks[i];
-        }
-        average = average / marks.length;
-         return average;
-        }
-   } 
-   let roundedAverage = Math.round(average);
-}
+   else { 
+      for (let i = 0; i < marks.length; i++){
+      averageMark = averageMark + marks[i];
+      }
+      averageMark = averageMark / marks.length;
+   }
+   let roundedAverage = Math.round(averageMark);
+
     return roundedAverage;
 }
+
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
