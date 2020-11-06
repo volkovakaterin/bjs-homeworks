af93890ccf9d588ed0e030479b03ce781c662f21
Ekaterina Volkova <volkovakaterin@gmail.com>
2020-11-06 19:40:11 +0400

    task.js changed

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -9,28 +9,25 @@ function parseCount(a) {
 function validateCount(a) {
     try {
         return parseCount(a);
-    } catch {
+    } catch(e) {
         return 'Невалидное значение';
     }
 }
 
 //Задача 2
-class Triangle {
-    constructor(a, b, c) {
+function Triangle(a, b, c) {
         this.a = a;
         this.b = b;
         this.c = c;
         this.P = a + b + c;
-        this.triangleRule = function () {
             if (a + b < c || a + c < b || b + c < a) {
-                throw new Error('Треугольник с такими сторонами не существует');
-            }
+                throw new Error('Треугольник с такими сторонами не существует');  
         }
-    }
-    getPerimeter() {
+    
+    this.getPerimeter = function() {
         return this.P;
     }
-    getArea() {
+    this.getArea = function() {
         let p = this.P / 2;
         let S = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
         S = S.toFixed(3);
@@ -38,16 +35,17 @@ class Triangle {
     }
 }
 
+
 function getTriangle(a, b, c) {
     try {
         return new Triangle(a, b, c);
-    } catch {
-        const TriangleFake = {
-            getPerimeter() {
-                return 'Ошибка! Треугольник не существует';
+    } catch(e) {
+        return TriangleFake = {
+            getPerimeter: function() {
+                 return 'Ошибка! Треугольник не существует';
             },
-            getArea() {
-                return 'Ошибка! Треугольник не существует';
+            getArea: function() {
+                 return 'Ошибка! Треугольник не существует';
             }
         }
     }
